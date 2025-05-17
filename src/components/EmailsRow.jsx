2552c9fc9dbade002fb5231c561824c212bb9d07
Nahid Khan <nahidkhanniloy31@gmail.com>
2025-05-17 17:21:03 +0600

    finished vedio 5

--- src/components/EmailsRow.jsx
+++ src/components/EmailsRow.jsx
@@ -1,8 +1,35 @@
 import React from "react";
-import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import { FaEdit, FaEye, FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const EmailsRow = ({ user, index }) => {
   const { _id, name, email, gender, status, photo } = user;
+  //   color
+  const statusClass =
+    status === "Active"
+      ? "text-green-600 font-semibold"
+      : "text-red-600 font-semibold";
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Swal.fire({
+        //   title: "Deleted!",
+        //   text: "Your file has been deleted.",
+        //   icon: "success",
+        // });
+      }
+    });
+  };
   return (
     <tr className="">
       <td className="text-blue-600 font-medium">{index + 1}</td>
@@ -19,13 +46,18 @@ const EmailsRow = ({ user, index }) => {
       <td>{name}</td>
       <td>{email}</td>
       <td>{gender}</td>
-      <td>{status}</td>
+      <td className={statusClass}>{status}</td>
 
       <td className="flex space-x-2 mt-3">
         <button className="btn btn-xs btn-outline text-purple-600">
-          <FaEdit />
+          <FaEye />
         </button>
         <button className="btn btn-xs btn-outline text-purple-600">
+          <FaEdit />
+        </button>
+        <button
+          onClick={() => handleDelete(_id)}
+          className="btn btn-xs btn-outline text-purple-600">
           <FaTrashAlt />
         </button>
       </td>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,14 +8,11 @@ const Home = () => {
   console.log(emails);
   return (
     <div className="min-h-screen bg-white border border-green-400 p-8">
-      {/* New User Button */}
-      <div className="mb-4">
-        <Link to="/addEmail">
-          <button className="btn btn-sm btn-outline btn-primary">
-            <FaUserPlus className="mr-2" />
-            Add New User
-          </button>
-        </Link>
+      <div className="mb-5 text-center">
+        <h1 className="text-green-600 font-bold text-2xl flex justify-center gap-1">
+          <FaUserPlus className="text-green-600" size={30} />
+          All Users Details
+        </h1>
       </div>
 
       {/* Table */}
@@ -38,6 +35,16 @@ const Home = () => {
             ))}
           </tbody>
         </table>
+      </div>
+
+      {/* New User Button */}
+      <div className="mb-4 mt-5">
+        <Link to="/addEmail">
+          <button className="btn btn-sm btn-outline btn-primary">
+            <FaUserPlus className="mr-2" />
+            Add New User
+          </button>
+        </Link>
       </div>
     </div>
   );
